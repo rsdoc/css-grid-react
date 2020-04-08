5db2ccd490c5a419e3c27a869550a551f0c40c23
Rahul Singh <50801861+rsdoc@users.noreply.github.com>
2020-04-08 21:53:34 +0530

    added initial grid setup

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="box box-1" />
+      <div className="box box-2" />
+      <div className="box box-3" />
+      <div className="box box-4" />
+      <div className="box box-5" />
+      <div className="box box-6" />
+      <div className="box box-7" />
+      <div className="box box-8" />
     </div>
   );
 }
